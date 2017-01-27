4b88c370f1f04a78d0d1146ca23df72ae7ec0d25
Biologique <zexi.pan@mail.utoronto.ca>
2017-01-26 20:26:31 -0500

    Themeable browser
    icons need to be placed within {root}\platforms\android\res. A picture
    for each different resolution.  Note: this is different between iOS and
    android.

--- myApp/src/pages/main/contact/contact.ts
+++ myApp/src/pages/main/contact/contact.ts
@@ -1,7 +1,6 @@
 import { Component} from '@angular/core';
-
 import { NavController  } from 'ionic-angular';
-import {InAppBrowser} from 'ionic-native';
+import {InAppBrowser, ThemeableBrowser} from 'ionic-native';
 
 
 @Component({
@@ -17,15 +16,40 @@ export class ContactPage {
 
   }
 
-
-
-  launch(){
-	let browser = new InAppBrowser('https://ehs.utoronto.ca/wp-content/uploads/2017/01/Chemical-Storage-Table-Updated.pdf', '_blank', 'location=yes');
-
-
-
+  launch_in_app(){
+	let browser = new InAppBrowser('https://ehs.utoronto.ca/wp-content/uploads/2017/01/Chemical-Storage-Table-Updated.pdf', '_blank', 'location=no');
   }
 
+  launch_themeable(){
+	  let options = {
+     statusbar: {
+         color: '#ffffffff'
+     },
+     toolbar: {
+         height: 44,
+         color: '#f0f0f0ff'
+     },
+     title: {
+         color: '#003264ff',
+         showPageTitle: true
+     },
+     backButton: {
+		image: 'ic_action_previous_item',
+        imagePressed: 'ic_action_previous_item',
+        align: 'left',
+        event: 'backPressed'
+		 
+		 
+   
+     },
+
+     backButtonCanClose: true
+};
+
+let browser = new ThemeableBrowser('https://docs.google.com/gview?embedded=true&url=ehs.utoronto.ca/wp-content/uploads/2017/01/Chemical-Storage-Table-Updated.pdf', '_blank', options);
+	  
+	  
+  }
 
 
 }
