e416ec71782cd4b0e34affae02649a3d2cbe227e
Biologique <zexi.pan@mail.utoronto.ca>
2017-01-31 13:45:37 -0500

    preliminary nfc merge
    update

--- myApp/src/pages/main/home/home.ts
+++ myApp/src/pages/main/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {ActivityPage} from '../../activity/activity';
-import {Platform, AlertController, NavController} from 'ionic-angular';
+import {AlertController, NavController, Platform, NavParams, ViewController } from 'ionic-angular';
+import { NFC, Ndef } from 'ionic-native';
 declare var cordova:any;
 
 @Component({
@@ -42,6 +43,58 @@ export class HomePage {
       });
     });
   }
+  
+    nfcPage() {
+    this.platform.ready().then(() => {
+      this.checkNFC();
+    });
+  }
+  
+  checkNFC() {
+    NFC.enabled()
+	//Success
+      .then(() => {
+        this.addListenNFC();
+      })
+	  //failure
+      .catch(err => {
+        console.log(err);
+        let alert = this.alert.create({
+          subTitle : "NFC DISABLED",
+          buttons: [{ text : "OK"},{ text : "Go Setting",
+            handler : () => {
+              NFC.showSettings();
+            }
+          }]
+        });
+        alert.present();
+      });
+  }
+  
+    addListenNFC() {
+   NFC.addNdefListener().subscribe(nfcData => {
+	   
+	   let message = JSON.stringify(nfcData);
+	  alert("Recieved NFC tag: " + message);
+	   
+	  
+		});
+  }
+
+  nfcWrite(){
+	  
+	  let message = Ndef.textRecord('Hello world');
+	  
+	  NFC.write([message])
+		.then( ()=> {
+			alert("success");
+		})
+		.catch( () =>{
+			alert("failure");
+		});
+  }
+  
+
 
 
 
